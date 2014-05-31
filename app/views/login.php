90eb7612af1e1ff21253b80347c59a67fc87af9c
Rob Johansen <robert_johans22@hotmail.com>
2014-05-31 14:15:39 -0600

    Add some jQuery for handling the submission of the login form via key press or button click.

--- app/views/login.php
+++ app/views/login.php
@@ -5,10 +5,25 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Tyedart - Login</title>
 	<link rel="stylesheet" href="/assets/css/tyedart-manage.css">
+	<script src="/assets/js/libs/jquery-min.js"></script>
+	<script type="text/javascript">
+		function submitLogin() {
+			$('#login-form').submit();
+		}
+		
+		$(function() {
+			$('#login-button').click(submitLogin);
+			$('#login-email, #login-password').keypress(function(e){
+				var keyCode = (e.keyCode ? e.keyCode : e.which);
+				if (keyCode == 13) {
+					submitLogin();
+				}
+			});
+		});
+	</script>
 </head>
 <body>
 	<div id="login-wrapper">
-		<?php if (isset($invalid)) echo '<p>Invalid credentials...</p>'; ?>
 		<form action="" method="post" id="login-form">
 			<input type="email" name="email" id="login-email" placeholder="Email Address" required autofocus />
 			<input type="password" name="password" id="login-password" placeholder="Password" required />
