da013e0f24a4b95550eb9faec2b88a0a02243f89
Rob Johansen <robert_johans22@hotmail.com>
2014-05-31 13:33:01 -0600

    Commit everything for a rather beautiful login page.

--- app/views/login.php
+++ app/views/login.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Tyedart - Login</title>
+	<link rel="stylesheet" href="/assets/css/tyedart-manage.css">
 </head>
 <body>
 	<div id="login-wrapper">
 		<?php if (isset($invalid)) echo '<p>Invalid credentials...</p>'; ?>
-		<form action="" method="post">
-			<input type="email" name="email" placeholder="Email Address" required autofocus />
-			<input type="password" name="password" placeholder="Password" required />
-			<input type="submit" value="Log In"/>
+		<form action="" method="post" id="login-form">
+			<input type="email" name="email" id="login-email" placeholder="Email Address" required autofocus />
+			<input type="password" name="password" id="login-password" placeholder="Password" required />
 		</form>
+		<div id="login-button-back"></div>
+		<div id="login-button" title="Authenticate"></div>
 	</div>
 </body>
 </html>
