3135788c6e2ad1bb8b5704d36af4a3388327bd9e
Rob Johansen <robert_johans22@hotmail.com>
2014-06-11 21:54:14 -0600

    Add some HTML and CSS for the prev/next arrows below the logos on the portfolio page.

--- public/assets/js/apps/portfolio-app.js
+++ public/assets/js/apps/portfolio-app.js
@@ -7,4 +7,13 @@ $(function() {
 	
 	// TODO: Create the models and views...Actually, maybe you should create the prev/next arrows since you got approval...
 	
+	/*
+	 	Mobile nav ideas for the portfolio page (using GSAP-JS):
+	 	
+	 	1. Add previous and next arrows below the four logos at the bottom.
+	 	2. When the next arrow is clicked, four new images animate in from the right.
+	 	3. As the four images move in, the page slowly scrolls up!
+	 	
+	 */
+	
 });
