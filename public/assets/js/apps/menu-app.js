cd27d13cf26f3f801792a6d3972c55629abeabe1
Rob Johansen <robert_johans22@hotmail.com>
2014-06-12 23:12:41 -0600

    Add links to the nav and make the selected link retain the hover bar.

--- public/assets/js/apps/menu-app.js
+++ public/assets/js/apps/menu-app.js
@@ -3,6 +3,13 @@ var app = app || {};
 
 $(function () {
 	
+	var url = document.URL;
+	if (/portfolio/.test(url))      $("#portfolio-link").addClass("selected")
+	else if (/quote/.test(url))     $("#quote-link").addClass("selected")
+	else if (/logoology/.test(url)) $("#logoology-link").addClass("selected")
+	else if (/resources/.test(url)) $("#resources-link").addClass("selected")
+	else if (/contact/.test(url))   $("#contact-link").addClass("selected")
+	
 	'use strict';
 	
 	var menuModel = new app.Menu();
@@ -16,5 +23,6 @@ $(function () {
 		model: menuModel,
 		el: "#mobile-nav"
 	});
+	
 	
 });
