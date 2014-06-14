3cf8f012e74fa66e0e904b19a63e9567d4535f26
Rob Johansen <robert_johans22@hotmail.com>
2014-06-14 07:36:22 -0600

    Commit working prototype of model/view code for the prev/next buttons on the portfolio page.

--- public/assets/js/apps/portfolio-app.js
+++ public/assets/js/apps/portfolio-app.js
@@ -8,34 +8,37 @@ $(function() {
 	app.PortfolioModel = Backbone.Model.extend({
 		
 		defaults: {
-			indexAtLogo1: 0, // index of the logo in the first (i.e. top left) position
-			indexAtLogo2: 1, // top right
-			indexAtLogo3: 2, // bottom left
-			indexAtLogo4: 3, // bottom right
+			indexAtLogo0: 0, // index of the logo in the first (i.e. top left) position
+			indexAtLogo1: 1, // top right
+			indexAtLogo2: 2, // bottom left
+			indexAtLogo3: 3, // bottom right
 			direction: ""    // direction from which logos should transition
 		},
 		
 		result: function() {
-			alert("Index at logo 1: "+this.get('indexAtLogo1')+"\n"+
+			alert("Index at logo 0: "+this.get('indexAtLogo0')+"\n"+
+				  "Index at logo 1: "+this.get('indexAtLogo1')+"\n"+
 				  "Index at logo 2: "+this.get('indexAtLogo2')+"\n"+
-				  "Index at logo 3: "+this.get('indexAtLogo3')+"\n"+
-				  "Index at logo 4: "+this.get('indexAtLogo4')+"\n");
+				  "Index at logo 3: "+this.get('indexAtLogo3')+"\n");
 		},
 		
 		prev: function() {
 			this.set("direction", "left");
 			
-			// WYLO...Fix this logic...you shouldn't always be subtracting 4 from app.Logos.length...just the wrap-around difference...
+			var indexAtLogo0 = this.get('indexAtLogo0') - 4;
 			
-			var indexAtLogo1 = this.get('indexAtLogo1') - 4 >= 0 ? this.get('indexAtLogo1') - 4 : app.Logos.length - 4;
+			if (indexAtLogo0 < 0) {
+				indexAtLogo0 = app.Logos.length + indexAtLogo0;
+			}
+			
+			var indexAtLogo1 = indexAtLogo0 + 1 < app.Logos.length ? indexAtLogo0 + 1 : 0;
 			var indexAtLogo2 = indexAtLogo1 + 1 < app.Logos.length ? indexAtLogo1 + 1 : 0;
 			var indexAtLogo3 = indexAtLogo2 + 1 < app.Logos.length ? indexAtLogo2 + 1 : 0;
-			var indexAtLogo4 = indexAtLogo3 + 1 < app.Logos.length ? indexAtLogo3 + 1 : 0;
 			
+			this.set("indexAtLogo0", indexAtLogo0);
 			this.set("indexAtLogo1", indexAtLogo1);
 			this.set("indexAtLogo2", indexAtLogo2);
 			this.set("indexAtLogo3", indexAtLogo3);
-			this.set("indexAtLogo4", indexAtLogo4);
 			
 			this.result();
 		},
@@ -43,15 +46,20 @@ $(function() {
 		next: function() {
 			this.set("direction", "right");
 			
-			var indexAtLogo1 = this.get('indexAtLogo1') + 4 < app.Logos.length ? this.get('indexAtLogo1') + 4 : 0;
+			var indexAtLogo0 = this.get('indexAtLogo0') + 4;
+			
+			if (indexAtLogo0 >= app.Logos.length) {
+				indexAtLogo0 = indexAtLogo0 - app.Logos.length;
+			}
+			
+			var indexAtLogo1 = indexAtLogo0 + 1 < app.Logos.length ? indexAtLogo0 + 1 : 0;
 			var indexAtLogo2 = indexAtLogo1 + 1 < app.Logos.length ? indexAtLogo1 + 1 : 0;
 			var indexAtLogo3 = indexAtLogo2 + 1 < app.Logos.length ? indexAtLogo2 + 1 : 0;
-			var indexAtLogo4 = indexAtLogo3 + 1 < app.Logos.length ? indexAtLogo3 + 1 : 0;
 			
+			this.set("indexAtLogo0", indexAtLogo0);
 			this.set("indexAtLogo1", indexAtLogo1);
 			this.set("indexAtLogo2", indexAtLogo2);
 			this.set("indexAtLogo3", indexAtLogo3);
-			this.set("indexAtLogo4", indexAtLogo4);
 			
 			this.result();
 		}
@@ -73,9 +81,27 @@ $(function() {
 		
 	});
 	
+	// next
+	var NextView = Backbone.View.extend({
+		
+		events: {
+			"click": "doNext"
+		},
+		
+		doNext: function() {
+			this.model.next();
+		}
+		
+	});
+	
 	new PrevView({
 		model: portfolioModel,
 		el: "#portfolio-nav-prev"
+	});
+	
+	new NextView({
+		model: portfolioModel,
+		el: "#portfolio-nav-next"
 	});
 	
 });
